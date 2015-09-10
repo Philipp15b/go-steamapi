260d8f5a3ac8f912276b260e22838a4b98cb08a9
vincentserpoul <vincent@serpoul.com>
2015-09-10 23:40:50 +0800

    add tradeoffer

--- tradeoffer.go
+++ tradeoffer.go
@@ -1,11 +1,84 @@
-package tradeoffer
+package steamapi
 
 import (
 	"encoding/json"
 	"fmt"
 	"net/http"
 	"net/url"
+	"strconv"
+
+	"github.com/vincentserpoul/mangosteam/inventory"
 )
+
+// State represents the state of the tradeoffer, see constants
+type State uint
+
+const (
+	// ETradeOfferStateCreated /!\ non steam status, used to know the TO has been created
+	ETradeOfferStateCreated State = iota
+	// ETradeOfferStateInvalid Invalid
+	ETradeOfferStateInvalid
+	// ETradeOfferStateActive This trade offer has been sent, neither party has acted on it yet.
+	ETradeOfferStateActive
+	// ETradeOfferStateAccepted The trade offer was accepted by the recipient and items were exchanged.
+	ETradeOfferStateAccepted
+	// ETradeOfferStateCountered The recipient made a counter offer
+	ETradeOfferStateCountered
+	// ETradeOfferStateExpired The trade offer was not accepted before the expiration date
+	ETradeOfferStateExpired
+	// ETradeOfferStateCanceled The sender cancelled the offer
+	ETradeOfferStateCanceled
+	// ETradeOfferStateDeclined The recipient declined the offer
+	ETradeOfferStateDeclined
+	// ETradeOfferStateInvalidItems Some of the items in the offer are no longer available
+	// (indicated by the missing flag in the output)
+	ETradeOfferStateInvalidItems
+	// ETradeOfferStateEmailPending The offer hasn't been sent yet and is awaiting email confirmation
+	ETradeOfferStateEmailPending
+	// ETradeOfferStateEmailCanceled The receiver cancelled the offer via email
+	ETradeOfferStateEmailCanceled
+)
+
+// CEconAsset represents an asset in steam web api
+type CEconAsset struct {
+	AppID      uint   `json:",string"`
+	ContextID  uint64 `json:",string"`
+	AssetID    uint64 `json:",string"`
+	CurrencyID uint64 `json:",string"`
+	ClassID    uint64 `json:",string"`
+	InstanceID uint64 `json:",string"`
+	Amount     uint64 `json:",string"`
+	Missing    bool
+}
+
+// CEconTradeOffer represent the to from the steam API
+type CEconTradeOffer struct {
+	TradeOfferID   uint64 `json:",string"`
+	OtherAccountID uint64 `json:"accountid_other"`
+	Message        string
+	ExpirationTime uint32        `json:"expiraton_time"`
+	State          State         `json:"trade_offer_state"`
+	ToGive         []*CEconAsset `json:"items_to_give"`
+	ToReceive      []*CEconAsset `json:"items_to_receive"`
+	IsOurs         bool          `json:"is_our_offer"`
+	TimeCreated    uint32        `json:"time_created"`
+	TimeUpdated    uint32        `json:"time_updated"`
+}
+
+// CEconTradeOffers contains a list of tradeoffers, sent and received
+type CEconTradeOffers struct {
+	Sent         []*CEconTradeOffer     `json:"trade_offers_sent"`
+	Received     []*CEconTradeOffer     `json:"trade_offers_received"`
+	Descriptions inventory.Descriptions `json:"descriptions"`
+}
+
+// SteamTradeOfferID is the identifier of the tradeoffer within steam network
+type SteamTradeOfferID uint64
+
+// String will turn a steamID into a string
+func (steamTradeOfferID SteamTradeOfferID) String() string {
+	return strconv.FormatUint(uint64(steamTradeOfferID), 10)
+}
 
 type ieconGetTradeOffersResponse struct {
 	Response CEconTradeOffers `json:"response"`
@@ -53,7 +126,7 @@ type ieconGetTradeOfferResponse struct {
 }
 
 // IEconGetTradeOffer retrieves details about a specific tradeoffer
-func IEconGetTradeOffer(baseSteamAPIURL string, apiKey string, steamID uint64, tradeOfferID SteamTradeOfferID) (
+func IEconGetTradeOffer(baseSteamAPIURL string, apiKey string, steamID uint64, tradeOfferID uint64) (
 	*CEconTradeOffer, error,
 ) {
 
@@ -61,9 +134,9 @@ func IEconGetTradeOffer(baseSteamAPIURL string, apiKey string, steamID uint64, t
 
 	querystring := url.Values{}
 	querystring.Add("key", apiKey)
-	querystring.Add("steamid", steamID.String())
+	querystring.Add("steamid", strconv.FormatUint(steamID, 10))
 	querystring.Add("format", "json")
-	querystring.Add("tradeofferid", tradeOfferID.String())
+	querystring.Add("tradeofferid", strconv.FormatUint(tradeOfferID, 10))
 	querystring.Add("language", "en")
 
 	resp, err := http.Get(baseSteamAPIURL + "/IEconService/GetTradeOffer/v0001?" + querystring.Encode())
