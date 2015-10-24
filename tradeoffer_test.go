35a41e29d5f838037751b0a0eaeb39f9e2befaf3
Jean-Jacques Serpoul <serpoul@gmail.com>
2015-10-24 13:52:41 +0200

    make GetMockActiveStateIEconGetTradeOffer public

--- tradeoffer_test.go
+++ tradeoffer_test.go
@@ -12,7 +12,7 @@ func TestIEconGetTradeOffer(t *testing.T) {
 		http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 			w.Header().Set("Content-Type", "application/json")
 			w.WriteHeader(http.StatusOK)
-			fmt.Fprintf(w, getMockActiveStateIEconGetTradeOffer())
+			fmt.Fprintf(w, GetMockActiveStateIEconGetTradeOffer())
 		}),
 	)
 
